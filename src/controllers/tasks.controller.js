ad3320fc4d79a20c521dcbb9aa1e6abaef7f6d1f
ManuelPaniagua <153279114+IanPaniagua@users.noreply.github.com>
2024-06-23 19:22:11 +0200

    Creating Controllers

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,18 @@
+export const getTasks = (req, res) => {
+    res.send('Hello World!')
+}
+export const createTask = (req, res) => {
+    res.send('NEW Tast!')
+}
+export const getTask = (req, res) => {
+    res.send('By Id!')
+}
+export const updateTask = (req, res) => {
+    res.send('Updating T!')
+}
+export const deleteTask = (req, res) => {
+    res.send('Deleting T!')
+}
+export const count = (req, res) => {
+    res.send('count!')
+}
