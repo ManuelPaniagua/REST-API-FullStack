7b938edb8f40f51df715f454cef1fd88c807305d
Manuel <m.p.porroa@maxedv-beratung.de>
2024-06-24 15:27:21 +0200

    testing request to the backend

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 // import morgan from "morgan";
 import taskRoutes from './src/routes/tasks.js';
@@ -22,28 +23,11 @@ const swaggerDocument = YAML.load(swaggerYAMLPath);
 // settings
 // app.set("port", process.env.PORT || 3000);
 
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "Tasks API",
-//       version: "1.0.0",
-//       description: "A simple express library API",
-//     },
-//     servers: [
-//       {
-//         url: "http://localhost:3000",
-//       },
-//     ],
-//   },
-//   apis: ["./src/routes/*.js"],
-// };
-// const specs = swaggerJsDoc(options);
-
 // middlewares
+app.use(cors()); //needed for http request
 app.use(express.json()); //save the data in the req.body
 // app.use(morgan("dev"));
-// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 // Serve Swagger documentation using Swagger UI
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
---
+++ frontend-app/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/task')
+      .then((response) => {
+        setTasks(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <h2>{task.name}</h2>
+          <p>{task.description}</p>
+        </li>
+      ))}
+    </div>
+  );
+};
+export default Home;
