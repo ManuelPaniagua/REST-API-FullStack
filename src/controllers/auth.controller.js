4fb0fccb1e4cb4be3e8e4aa8636f2455d758b673
Manuel <m.p.porroa@maxedv-beratung.de>
2024-06-26 11:00:55 +0200

    Bcryptjs, passwordHashed for the register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,23 @@
 import { getConnection } from '../database.js';
 import { v4 } from 'uuid';
+import bcryptjs from 'bcryptjs';
 
 export const registerUser = async (req, res) => {
-  const newUser = {
-    id: v4(),
-    email: req.body.email,
-    password: req.body.password,
-    username: req.body.username,
-  };
+  const { email, password, username } = req.body;
+
   try {
+    const passwordHash = await bcryptjs.hash(password, 10);
+    const newUser = {
+      id: v4(),
+      email: req.body.email,
+      password: passwordHash,
+      username: req.body.username,
+    };
     const db = getConnection();
     db.data.users.push(newUser);
     await db.write();
     console.log(newUser);
+
     res.json(newUser);
   } catch (error) {
     return res.status(500).send(error);
