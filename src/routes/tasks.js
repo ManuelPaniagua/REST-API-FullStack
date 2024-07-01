25188deb8b02ef22257822dc66c6874617585af1
Manuel <m.p.porroa@maxedv-beratung.de>
2024-07-01 13:09:14 +0200

    validating Task Post

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -9,13 +9,20 @@ import {
     updateTask,
 } from '../controllers/tasks.controller.js';
 import { authRequired } from '../middlewares/validateToken.js';
+import { validateSchema } from '../middlewares/validator.middleware.js';
+import { createTaskSchema } from '../schemas/task.schema.js';
 
 const router = Router();
 
 router.get('/task', authRequired, getTasks);
 router.get('/task/count', authRequired, countTasks);
 router.get('/task/:id', authRequired, getTask);
-router.post('/task', authRequired, createTask);
+router.post(
+    '/task',
+    authRequired,
+    validateSchema(createTaskSchema),
+    createTask,
+);
 router.put('/task/:id', authRequired, updateTask);
 router.delete('/task/:id', authRequired, deleteTask);
 router.delete('/task/delete/all', authRequired, deleteAllTasks);
