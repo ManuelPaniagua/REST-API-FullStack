b3c7b169d695cdf3887682eefca24bbec67a7e85
Manuel <m.p.porroa@maxedv-beratung.de>
2024-07-01 12:41:00 +0200

    Schema for Authorization (register, login)

---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,40 @@
+import { z } from 'zod';
+
+//to validate the user register
+export const registerSchema = z.object({
+    username: z.string({
+        required_error: 'Username is required',
+    }),
+    email: z
+        .string({
+            required_error: 'Email is required',
+        })
+        .email({
+            message: 'Invalid email',
+        }),
+    password: z
+        .string({
+            required_error: 'Password is required',
+        })
+        .min(6, {
+            message: 'Password must be at least 6 charachters',
+        }),
+});
+
+//to validate login
+export const loginSchema = z.object({
+    email: z
+        .string({
+            required_error: 'Email is required',
+        })
+        .email({
+            message: 'Invalid email',
+        }),
+    password: z
+        .string({
+            required_error: 'Password is required',
+        })
+        .min(6, {
+            message: 'Password must be at east 6 charachters',
+        }),
+});
